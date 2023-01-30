46dcf9bdf2ce3ba62fc80c90cf8b6cc99204b0cf
Eder-BL <eder.bergmann@gmail.com>
2023-01-29 21:42:55 -0300

    Aula 1 completada

---
+++ lib/app/pages/splash/splash_page.dart
@@ -0,0 +1,25 @@
+import 'package:dw9_delivery_app/app/core/config/env/env.dart';
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
+      ),
+      body: Column(
+        children: [
+          DeliveryButton(
+              onPressed: () {}, label: Env.i['backend_base_url'] ?? ''),
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'Teste'),
+          )
+        ],
+      ),
+    );
+  }
+}
