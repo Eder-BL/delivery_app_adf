a6b6253789d7f9b2408c7c3ece5fea1c147c928e
Eder-BL <eder.bergmann@gmail.com>
2023-02-01 23:57:09 -0300

    Aula2 - Parte1 finalizada

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:dw9_delivery_app/app/models/product_model.dart';
+import 'package:dw9_delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,11 +9,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text('Home Page'),
+      appBar: DeliveryAppbar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return DeliveryProductTile(
+                  product: ProductModel(
+                    id: 0,
+                    name: 'Nome',
+                    description: 'descrip',
+                    price: 15.00,
+                    image:
+                        'https://t1.uc.ltmcdn.com/pt/posts/0/1/3/como_fazer_sanduiche_natural_9310_600.webp',
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
