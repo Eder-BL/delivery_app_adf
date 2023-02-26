79827ac1cf025a6496c423e10fa68ba7f76a0f5f
Eder-BL <eder.bergmann@gmail.com>
2023-02-26 12:05:12 -0300

    Aula 3 - Parte 2 00:35:55

--- lib/app/pages/home/widgets/delivery_product_tile.dart
+++ lib/app/pages/home/widgets/delivery_product_tile.dart
@@ -1,24 +1,37 @@
 import 'package:dw9_delivery_app/app/core/extensions/formatter_extension.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/colors_app.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
+import 'package:dw9_delivery_app/app/dto/order_product_dto.dart';
 import 'package:dw9_delivery_app/app/models/product_model.dart';
+import 'package:dw9_delivery_app/app/pages/home/home_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DeliveryProductTile extends StatelessWidget {
-  ProductModel product;
+  final ProductModel product;
+  final OrderProductDto? orderProduct;
 
-  DeliveryProductTile({
+  const DeliveryProductTile({
     super.key,
     required this.product,
+    required this.orderProduct,
   });
 
   @override
   Widget build(BuildContext context) {
+    final controller = context.read<HomeController>();
+
     return InkWell(
       onTap: () async {
-        await Navigator.of(context).pushNamed('/productDetail', arguments: {
+        final orderProductResult =
+            await Navigator.of(context).pushNamed('/productDetail', arguments: {
           'product': product,
+          'order': orderProduct,
         });
+
+        if (orderProductResult != null) {
+          controller.addOrUpdateBag(orderProductResult as OrderProductDto);
+        }
       },
       child: Padding(
         padding: const EdgeInsets.all(8.0),
--- lib/app/pages/product_detail/product_detail_page.dart
+++ lib/app/pages/product_detail/product_detail_page.dart
@@ -2,6 +2,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:dw9_delivery_app/app/core/extensions/formatter_extension.dart';
 import 'package:dw9_delivery_app/app/core/ui/base_state/base_state.dart';
+import 'package:dw9_delivery_app/app/dto/order_product_dto.dart';
 import 'package:flutter/material.dart';
 
 import 'package:dw9_delivery_app/app/core/ui/helpers/size_extentions.dart';
@@ -14,8 +15,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductDetailPage extends StatefulWidget {
   final ProductModel product;
+  final OrderProductDto? order;
 
-  const ProductDetailPage({super.key, required this.product});
+  const ProductDetailPage({
+    super.key,
+    required this.product,
+    this.order,
+  });
 
   @override
   State<ProductDetailPage> createState() => _ProductDetailPageState();
@@ -23,6 +29,13 @@ class ProductDetailPage extends StatefulWidget {
 
 class _ProductDetailPageState
     extends BaseState<ProductDetailPage, ProductDetailController> {
+  @override
+  void initState() {
+    super.initState();
+    final amount = widget.order?.amount ?? 1;
+    controller.initial(amount, widget.order != null);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,7 +90,7 @@ class _ProductDetailPageState
                       decrementTap: () {
                         controller.decrement();
                       },
-                      amout: amount,
+                      amount: amount,
                     );
                   },
                 ),
@@ -89,29 +102,52 @@ class _ProductDetailPageState
                 child: BlocBuilder<ProductDetailController, int>(
                   builder: (context, amount) {
                     return ElevatedButton(
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Adicionar',
-                            style: context.textStyles.textExtraBold
-                                .copyWith(fontSize: 13),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Expanded(
-                            child: AutoSizeText(
-                              maxFontSize: 13,
-                              minFontSize: 5,
-                              maxLines: 1,
-                              textAlign: TextAlign.center,
-                              (widget.product.price * amount).currencyPTBR,
-                              style: context.textStyles.textExtraBold,
+                      style: amount == 0
+                          ? ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red)
+                          : null,
+                      onPressed: () {
+                        if (amount == 0) {
+                          _showConfirmDelete(amount);
+                        } else {
+                          Navigator.of(context).pop(
+                            OrderProductDto(
+                              product: widget.product,
+                              amount: amount,
                             ),
-                          ),
-                        ],
+                          );
+                        }
+                      },
+                      child: Visibility(
+                        visible: amount > 0,
+                        replacement: Text(
+                          'Excluir produto',
+                          style: context.textStyles.textExtraBold
+                              .copyWith(fontSize: 13),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Adicionar',
+                              style: context.textStyles.textExtraBold
+                                  .copyWith(fontSize: 13),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                              child: AutoSizeText(
+                                maxFontSize: 13,
+                                minFontSize: 5,
+                                maxLines: 1,
+                                textAlign: TextAlign.center,
+                                (widget.product.price * amount).currencyPTBR,
+                                style: context.textStyles.textExtraBold,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
@@ -122,5 +158,43 @@ class _ProductDetailPageState
         ],
       ),
     );
+  }
+
+  void _showConfirmDelete(int amount) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Deseja excluir o produto?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Cancelar',
+                  style:
+                      context.textStyles.textBold.copyWith(color: Colors.red),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.of(context).pop(
+                    OrderProductDto(
+                      product: widget.product,
+                      amount: amount,
+                    ),
+                  );
+                },
+                child: Text(
+                  'Confirmar',
+                  style: context.textStyles.textBold,
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
